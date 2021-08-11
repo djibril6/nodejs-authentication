af8014be614a897b664565e1be1d241d95989293
Djibril <issoufoudjib@gmail.com>
2021-08-11 18:50:51 +0300

    route for Email verification, password reinitialisation, update password

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import { Request, Response } from 'express';
 import { authController } from '../controllers';
 import { validate } from '../middlewares';
 import { authValidation } from '../validations';
@@ -10,9 +9,12 @@ authRoute.post('/register',  validate(authValidation.register), authController.r
 authRoute.post('/email-login',  validate(authValidation.loginWithEmail), authController.loginWithEmail);
 authRoute.post('/tel-login',  validate(authValidation.loginWithTel), authController.loginWithTel);
 authRoute.post('/logout', validate(authValidation.logout), authController.logout);
-
-// authRoute.get('/verify-email/:token', async (req: Request, res: Response) => {
-//   authController.verifyEmail(req, res);
-// });
+authRoute.get('/verify-email/:token', validate(authValidation.verifyEmail), authController.verifyEmail);
+authRoute.post('/send-email-verification', 
+    validate(authValidation.resendVerificationEmail), 
+    authController.resendVerificationEmail);
+authRoute.post('/reset-token', validate(authValidation.refreshTokens), authController.refreshTokens);
+authRoute.post('/forgot-password', validate(authValidation.forgotPassword), authController.forgotPassword);
+authRoute.patch('/update-password', validate(authValidation.resetPassword), authController.resetPassword);
 
 export default authRoute;
